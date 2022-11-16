198ed2c09c2439bf6bf06262f7e8aeaaff57bdd3
SamWarden <SamWardenSad@gmail.com>
2022-11-16 13:25:01 +0300

    Add QueryDispatcher interface to didiator

--- didiator/interface/dispatcher.py
+++ didiator/interface/dispatcher.py
@@ -1,9 +1,12 @@
 from typing import Any, Awaitable, Callable, ParamSpec, Protocol, Type, TypeVar
 
 from didiator.command import Command, CommandHandler
+from didiator.query import Query
 
 CR = TypeVar("CR")
-C = TypeVar("C", bound=Command)
+QR = TypeVar("QR")
+C = TypeVar("C", bound=Command[Any])
+Q = TypeVar("Q", bound=Query[Any])
 P = ParamSpec("P")
 
 HandlerType = Callable[[C], Awaitable[CR]] | Type[CommandHandler[C, CR]]
@@ -28,4 +31,20 @@ class CommandDispatcher(Protocol):
         raise NotImplementedError
 
     async def send(self, command: Command[CR], *args: P.args, **kwargs: P.kwargs) -> CR:
+        raise NotImplementedError
+
+
+class QueryDispatcher(Protocol):
+    @property
+    def handlers(self) -> dict[Type[Command[Any]], HandlerType[Any, Any]]:
+        raise NotImplementedError
+
+    @property
+    def middlewares(self) -> tuple[MiddlewareType[Any, Any], ...]:
+        raise NotImplementedError
+
+    def register_handler(self, query: Type[Q], handler: HandlerType[Q, QR]) -> None:
+        raise NotImplementedError
+
+    async def query(self, query: Query[QR], *args: P.args, **kwargs: P.kwargs) -> QR:
         raise NotImplementedError
