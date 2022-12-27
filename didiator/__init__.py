63d4e05e80601aa45b9097d340d158c58d05a39c
SamWarden <SamWardenSad@gmail.com>
2022-12-27 15:49:23 +0300

    Add the EventObserver to the Mediator implementation

--- didiator/__init__.py
+++ didiator/__init__.py
@@ -1,7 +1,9 @@
 from .dispatchers import CommandDispatcherImpl, QueryDispatcherImpl
-from .interface.entities import Command, Query
+from .interface import CommandDispatcher, EventHandler, EventObserver, QueryDispatcher
+from .observers import EventObserverImpl
+from .interface.entities import Command, Query, Event
 from .interface.handlers import CommandHandler, QueryHandler
-from .interface.mediator import Mediator
+from .interface.mediator import Mediator, CommandMediator, QueryMediator, EventMediator
 from .mediator import MediatorImpl
 
 __version__ = "0.1.1"
@@ -10,10 +12,19 @@ __all__ = (
     "__version__",
     "MediatorImpl",
     "Mediator",
+    "CommandMediator",
+    "QueryMediator",
+    "EventMediator",
     "Command",
     "CommandHandler",
+    "CommandDispatcher",
     "CommandDispatcherImpl",
     "Query",
     "QueryHandler",
+    "QueryDispatcher",
     "QueryDispatcherImpl",
+    "Event",
+    "EventHandler",
+    "EventObserver",
+    "EventObserverImpl",
 )
