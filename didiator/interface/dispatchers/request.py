10256f169aa8c06e5f69fcd36b55158e69a5d9d2
SamWarden <SamWardenSad@gmail.com>
2023-01-31 02:02:21 +0300

    Add `copy` method to dispatchers and the `EventObserver`

--- didiator/interface/dispatchers/request.py
+++ didiator/interface/dispatchers/request.py
@@ -4,6 +4,7 @@ from didiator.interface.entities.request import Request
 from didiator.interface.handlers import HandlerType
 from didiator.middlewares.base import MiddlewareType
 
+Self = TypeVar("Self", bound="Dispatcher")
 R = TypeVar("R", bound=Request[Any])
 RRes = TypeVar("RRes")
 
@@ -15,4 +16,7 @@ class Dispatcher(Protocol):
 
     @property
     def middlewares(self) -> tuple[MiddlewareType[Request[Any], Any], ...]:
+        raise NotImplementedError
+
+    def copy(self: Self) -> Self:
         raise NotImplementedError
